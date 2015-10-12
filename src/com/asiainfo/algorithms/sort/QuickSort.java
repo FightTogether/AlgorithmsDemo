77ccba44ed0d9fc7e76c9c183d98edf7d49581af
FightTogether <wenghy@yeah.net>
2015-10-12 21:05:44 +0800

    修改上次错误

--- src/com/asiainfo/algorithms/sort/QuickSort.java
+++ src/com/asiainfo/algorithms/sort/QuickSort.java
@@ -4,37 +4,36 @@ import com.asiainfo.algorithms.utils.ArrayUtils;
 
 public class QuickSort {
 	public static void sort(int[] array) {
-		quicksort(array, 1, array.length-1);
+		quicksort(array, 0, array.length-1);
 	}
 
 	public static void quicksort(int[] array, int l, int r) {
 		if(l>r)return;
-		int i = l, j = r;
-		int x = array[l];//x中存的就是基准数
-		while (i != j) {
+		int i=l,j=r,x=array[l];//x中存的就是基准数 
+		while(i!=j){
 			//顺序很重要，要先从右边开始找 
-			while (i < j && array[j] >= x)j--;
-			//再找右边的 
-			while (i < j && array[i] <= x)i++;
-			// 交换两个数在数组中的位置
-			if (i < j) {
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
+			while(i<j&&array[j]>=x)j--;
+			 //再找右边的 
+			while(i<j&&array[i]<=x)i++;
+			 //交换两个数在数组中的位置 
+			 if(i<j)swap(array, i, j);
 		}
-		//最终将基准数归位 
-		array[l] = array[i];
-		array[i]=x;
-		
-		quicksort(array,0,i-1);//继续处理左边的，这里是一个递归的过程     
-		quicksort(array,i+1,r);//继续处理右边的 ，这里是一个递归的过程 
+	    //最终将基准数归位 
+		 swap(array, l, i);
+		//继续处理左边的，这里是一个递归的过程 
+		 quicksort(array, l, i-1);
+		//继续处理右边的 ，这里是一个递归的过程 
+		 quicksort(array, i+1, r);
+	}
+	public static void swap(int[] array,int idxA,int idxB){
+		int temp=array[idxA];
+		array[idxA]=array[idxB];
+		array[idxB]=temp;
 	}
 	public static void main(String[] args) {
 		int[] array=ArrayUtils.generateNoRepeatArray(20, 40, 10);
 		ArrayUtils.printf(array);
 		sort(array);
 		ArrayUtils.printf(array);
-		
 	}
 }
