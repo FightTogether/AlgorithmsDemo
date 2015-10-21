ce9a22c38c3710f61ab0137b15189da5d19b5184
FightTogether <wenghy@yeah.net>
2015-10-21 21:14:07 +0800

    归并排序

--- src/com/asiainfo/algorithms/sort/MergeSort.java
+++ src/com/asiainfo/algorithms/sort/MergeSort.java
@@ -3,60 +3,36 @@ package com.asiainfo.algorithms.sort;
 import com.asiainfo.algorithms.utils.ArrayUtils;
 
 public class MergeSort {
-	public static void sort(int[] array) {
-		int[] aryA = ArrayUtils.generateNoRepeatArray(10, 50, 10);
-		int[] aryB = ArrayUtils.generateNoRepeatArray(50, 80, 10);
-		ArrayUtils.printf(mergeAry(aryA, aryB));
-	}
 
-	public static int[] mergeAry(int[] aryA, int[] aryB) {
-		int lenA = aryA.length;
-		int lenB = aryB.length;
-		int[] aryC = new int[lenA + lenB];
-		int i = 0, j = 0;
-		for (int m = 0, n = aryC.length; m < n; m++) {
-			if (j >= lenB || (i < lenA && aryA[i] <= aryB[j])) {
-				aryC[m] = aryA[i];
-				i++;
-			} else {
-				aryC[m] = aryB[j];
-				j++;
-			}
-		}
-		return aryC;
-	}
-
-	public static void merge(int[] array, int mid) {
-
-		for (int i = 0, n = array.length; i < n; i++) {
-
-		}
-	}
 
 	public static void main(String[] args) {
 		int[] data = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };
 		ArrayUtils.printf(data);
-		mergeSort(data);
 		System.out.println("排序后的数组：");
 		ArrayUtils.printf(data);
 	}
-
-	public static void mergeSort(int[] data) {
-		sort(data, 0, data.length - 1);
+/**
+ * 排序函数
+ * @param data
+ */
+	public static void sort(int[] data) {
+		sort(data, 0, data.length-1);
 	}
-
+	/**
+	 * 分治法，对子数组进行排序
+	 */
 	public static void sort(int[] data, int left, int right) {
-		if (left >= right)
-			return;
-		// 找出中间索引
-		int center = (left + right) / 2;
-		// 对左边数组进行递归
+		//找出中间点
+		int center=(left+right)/2;
+		//当left大于等于right时 停止(非常重要，程序停止的条件)
+		//也就是，当无法再往下分的时候，就停止
+		if (left >= right)return;
+		//对左边进行排序
 		sort(data, left, center);
-		// 对右边数组进行递归
-		sort(data, center + 1, right);
-		// 合并
+		//对右边进行排序
+		sort(data, center+1, right);
+		//合并排序后的左右子数组
 		merge(data, left, center, right);
-		ArrayUtils.printf(data);
 	}
 
 	/**
@@ -72,39 +48,34 @@ public class MergeSort {
 	 *            右数组最后一个元素的索引
 	 */
 	public static void merge(int[] data, int left, int center, int right) {
-		// 临时数组
-		int[] tmpArr = new int[data.length];
-		// 右数组第一个元素索引
-		int mid = center + 1;
-		// third 记录临时数组的索引
-		int third = left;
-		// 缓存左数组第一个元素的索引
-		int tmp = left;
-		while (left <= center && mid <= right) {
-			// 从两个数组中取出最小的放入临时数组
-			if (data[left] <= data[mid]) {
-				tmpArr[third++] = data[left++];
-			} else {
-				tmpArr[third++] = data[mid++];
+		//临时数组，用于存储排序后的数据
+		int[] tmpAry=new int[data.length];
+		//tmpIdx当前临时数组的左边标识  markIdx为左边坐标的拷贝，在最后要把排序后的临时数组复制回原数组时有用
+		int tmpIdx=left,markIdx=left;
+		//中间index 做右边子数组的小边界
+		int mid=center+1;
+		//但两个子数组有一个比较完时，就可跳出循环
+		while(left<=center&& mid<=right){
+			if(data[left]<=data[mid]){
+				tmpAry[tmpIdx++]=data[left++];
+			}else{
+				tmpAry[tmpIdx++]=data[mid++];
+
 			}
 		}
-		// 剩余部分依次放入临时数组（实际上两个while只会执行其中一个）
-		while (mid <= right) {
-			tmpArr[third++] = data[mid++];
+		//判断左边是否没走完，没走完则直接全部把剩余复制给临时数组(其实两个只能走一个)
+		while(left<=center){
+			tmpAry[tmpIdx++]=data[left++];
+		}		
+		//判断右边是否没走完，没走完则直接全部把剩余复制给临时数组(其实两个只能走一个)
+		while(mid<=right){
+			tmpAry[tmpIdx++]=data[mid++];
 		}
-		while (left <= center) {
-			tmpArr[third++] = data[left++];
+		//把临时数组(已经排好序的)全部复制回原数组
+		while(markIdx<=right){
+			data[markIdx]=tmpAry[markIdx++];
 		}
-		// 将临时数组中的内容拷贝回原数组中
-		// （原left-right范围的内容被复制回原数组）
-		while (tmp <= right) {
-			data[tmp] = tmpArr[tmp++];
-		}
+		
 	}
 
-	public static void swap(int[] array, int idxA, int idxB) {
-		int temp = array[idxA];
-		array[idxA] = array[idxB];
-		array[idxB] = temp;
-	}
 }
