a51c937fb54d16c58d73fd58740c8185318285ac
FightTogether <wenghy@yeah.net>
2015-10-27 22:16:41 +0800

    全量提交

---
+++ src/com/asiainfo/algorithms/findduplicate/Solution.java
@@ -0,0 +1,21 @@
+package com.asiainfo.algorithms.findduplicate;
+
+public class Solution {
+	 public int findDuplicate(int[] nums) {
+		 for(int i=0, n=nums.length;i<n;i++){
+			 int num=nums[i];
+			 for(int j=i+1;j<n;j++){
+				 if(num==nums[j]){
+					 return num;
+				 }
+			 }
+		 }
+		 throw new RuntimeException("not find the duplicate number");
+	    }
+	 public static void main(String[] args) {
+		 Solution solution=new Solution();
+		 int[] nums=new int[]{1,2,3,4,5,5,6,7};
+		 int duplicate=solution.findDuplicate(nums);
+		 System.out.println(duplicate);
+	}
+}
---
+++ src/com/asiainfo/algorithms/medianfiinder/MedianFinder.java
@@ -0,0 +1,51 @@
+package com.asiainfo.algorithms.medianfiinder;
+
+import com.asiainfo.algorithms.utils.ArrayUtils;
+
+class MedianFinder {
+	int[] colection=null;
+	int length=0;
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+    	if(length==0){
+    		colection=new int[1];
+    		colection[0]=num;
+    		length=1;
+    		return ;
+    	}
+    	
+    	int[] temp=new int[length+1];
+    	int index=0;
+    	for(int i=0;i<length;i++){
+    		if(index==i&&colection[i]>num){
+    			temp[index++]=num;
+    		}
+    		temp[index++]=colection[i];
+    	}
+    	if(index==length){
+    		temp[index]=num;
+    	}
+    	colection=temp;
+    	length=temp.length;
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+    	if(length==0){
+    		throw new RuntimeException("长度不能为空");
+    	}
+    	if(length%2==1){
+    		return colection[length/2];
+    	}else{
+    		return (colection[length/2-1]+colection[length/2])/2.00;
+    	}
+    }
+    public void printf() {
+    	ArrayUtils.printf(colection);
+	}
+};
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
