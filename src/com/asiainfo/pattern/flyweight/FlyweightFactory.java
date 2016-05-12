073422ca20637871804bf5f9fc8c85098b14832e
wenghy <silentpoppy@foxmail.com>
2016-05-12 23:03:35 +0800

    提交设计模式

---
+++ src/com/asiainfo/pattern/flyweight/FlyweightFactory.java
@@ -0,0 +1,15 @@
+package com.asiainfo.pattern.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlyweightFactory {
+    private static Map<Character,Flyweight> files = new HashMap<Character,Flyweight>();
+    
+    public static Flyweight factory(Character state){
+    	if (!files.containsValue(state)) {
+    		files.put(state, new ConcreteFlyweight(state));
+		}
+    	return files.get(state);
+    }
+}
