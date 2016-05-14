34e3a37d8c2a248a55ed101c27eb1740bea8700f
wenghy <silentpoppy@foxmail.com>
2016-05-14 21:55:48 +0800

    添加复合享元模式

---
+++ src/com/asiainfo/pattern/flyweight/CompositeFlyweightFactory.java
@@ -0,0 +1,21 @@
+package com.asiainfo.pattern.flyweight;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class CompositeFlyweightFactory {
+	private static HashMap<Character,Flyweight> files=new HashMap<Character,Flyweight>();
+	public static Flyweight factory(Character state){
+		if(!files.containsKey(state)){
+			files.put(state, new ConcreteFlyweight(state));
+		}
+		return files.get(state);
+	}
+	public static Flyweight factory(List<Character> states){
+		ConcreteCompositeFlyweight flyweight=new ConcreteCompositeFlyweight();
+		for(Character state:states){
+			flyweight.add(state, factory(state));
+		}
+		return flyweight;
+	}
+}
